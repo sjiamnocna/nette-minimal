7b56158771f5f539b5b45b15650ff29423a96f3f
sjiamnocna <jancikuvmail@gmail.com>
2021-03-17 21:53:44 +0100

    Do basic checks before letting client in

--- www/index.php
+++ www/index.php
@@ -1,5 +1,19 @@
 <?php declare(strict_types=1);
 
+define('PROTO', 'https://');
+define('SITE_ROOT_URI', 'example.cz');
+
+// do basic checks before doing anything
+
+if((empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')
+	|| (empty($_SERVER['HTTP_REFERER']) || $_SERVER['HTTP_REFERER'] == PROTO . SITE_ROOT_URI)) { 
+	// this is not AJAX call or it came from different site than allowed
+	header('HTTP/1.0 403 Forbidden');
+	die('403 - Forbidden');
+}
+
+// now get required tools and run the application
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/bootstrap.php';
 
